3f80a565bd4ad30f17455a162be1ee56382f2b72
Komesu, D.K <daniel@dkko.me>
2025-06-08 15:32:11 -0300

    Group RAIS-vinculos data by year when converting to parquet

--- data-wrangling/0-raw-to-parquet/1-decompress-convert-rais.py
+++ data-wrangling/0-raw-to-parquet/1-decompress-convert-rais.py
@@ -9,20 +9,33 @@ from pdet_data import reader
 DIR = Path(__file__).parent.absolute()
 
 
-def convert_rais(
-    decompressed_filepath: Path, dataset: str, dest_filepath: Path, year: int | str
-):
-    try:
+def convert_rais_directory(data_raw_rais_year_dir: Path, dataset: str, dest_intermediary_rais_dir: Path):
+    year = int(data_raw_rais_year_dir.name)
+    print(f"Converting RAIS {dataset} data for year {year}...")
+    files = []
+    for file in data_raw_rais_year_dir.iterdir():
+        file_metadata = reader.parse_filename(file)
+        files.append(file_metadata)
+
+    latest_modification = max(file_metadata["modification"] for file_metadata in files)
+
+    dest_filepath = dest_intermediary_rais_dir / f"{dataset}_{year}@{latest_modification}.parquet"
+    if dest_filepath.exists():
+        return
+
+    data = pl.DataFrame()
+    for file_metadata in files:
+        decompressed = reader.decompress(file_metadata)
+        decompressed_filepath = decompressed["decompressed_filepath"]
         df = reader.read_rais(
             decompressed_filepath,
             year=year,
             dataset=dataset,
         )
-        reader.write_parquet(df, dest_filepath)
-    except pl.exceptions.ComputeError as e:
-        print(f"Error converting {decompressed_filepath}: {e}")
-    except pl.exceptions.ShapeError as e:
-        print(f"Error converting {decompressed_filepath}: {e}")
+        data = pl.concat([data, df], how="vertical")
+        shutil.rmtree(decompressed["tmp_dir"])
+
+    reader.write_parquet(data, dest_filepath)
 
 
 def main():
@@ -39,33 +52,15 @@ def main():
     dataset = "vinculos"
     data_dir_rais_vinculos = data_dir / "rais-vinculos"
     dest_dir_rais_vinculos = dest_dir / "rais-vinculos"
-    for file in data_dir_rais_vinculos.glob("**/rais-*.*"):
-        file_metadata = reader.parse_filename(file)
-        date = file_metadata["date"]
-        name = file_metadata["name"]
-        dest_filepath = dest_dir_rais_vinculos / str(date) / f"{name}.parquet"
-        if dest_filepath.exists():
-            continue
-        decompressed = reader.decompress(file_metadata)
-        decompressed_filepath = decompressed["decompressed_filepath"]
-        convert_rais(decompressed_filepath, dataset, dest_filepath, date)
-        shutil.rmtree(decompressed["tmp_dir"])
+    for data_raw_rais_year_dir in data_dir_rais_vinculos.iterdir():
+        convert_rais_directory(data_raw_rais_year_dir, dataset, dest_dir_rais_vinculos)
 
     # rais-estabelecimentos
     dataset = "estabelecimentos"
     data_dir_rais_estabelecimentos = data_dir / "rais-estabelecimentos"
     dest_dir_rais_estabelecimentos = dest_dir / "rais-estabelecimentos"
-    for file in data_dir_rais_estabelecimentos.glob("**/rais-*.*"):
-        file_metadata = reader.parse_filename(file)
-        date = file_metadata["date"]
-        name = file_metadata["name"]
-        dest_filepath = dest_dir_rais_estabelecimentos / str(date) / f"{name}.parquet"
-        if dest_filepath.exists():
-            continue
-        decompressed = reader.decompress(file_metadata)
-        decompressed_filepath = decompressed["decompressed_filepath"]
-        convert_rais(decompressed_filepath, dataset, dest_filepath, date)
-        shutil.rmtree(decompressed["tmp_dir"])
+    for data_raw_rais_year_dir in data_dir_rais_estabelecimentos.iterdir():
+        convert_rais_directory(data_raw_rais_year_dir, dataset, dest_dir_rais_estabelecimentos)
 
     print("Done converting RAIS files.")
 
